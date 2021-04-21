16fd9889c3a1f2e92208c03b5e88aad6f7d9d6b6
Juan Feris <juan.feris@gorillalogic.com>
2021-04-21 17:09:24 -0500

    Initial Commit and Posts Server

---
+++ posts/index.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import { randomBytes } from 'crypto'
+
+const posts = {}
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+	res.send(posts)
+})
+
+app.post('/', (req, res) => {
+	const id = randomBytes(4).toString('hex')
+	const { title } = req.body
+	console.log(req.body)
+	posts[id] = { id, title }
+	res.status(201).send(posts[id])
+})
+
+app.listen(4000, () => {
+	console.log('Listening on 4000')
+})
