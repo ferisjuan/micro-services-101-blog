2a024c16697c6e0710f9f5e9730fd608283cd8ce
Juan Feris <juan.feris@gorillalogic.com>
2021-04-22 09:43:27 -0500

    Create Post Form

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,10 @@
-export default () => <div>Blog App</div>
+import CreatePost from './CreatePost'
+
+export default () => {
+	return (
+		<div>
+			<h1>Create Post</h1>
+			<CreatePost />
+		</div>
+	)
+}
