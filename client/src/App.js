0b8e1a80b08dc4ad5eed55df49bb53df7936d114
Juan Feris <juan.feris@gorillalogic.com>
2021-04-23 16:23:33 -0500

    Rendering Posts

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import CreatePost from './CreatePost'
+import PostList from './PostList'
 
 export default () => {
 	return (
-		<div>
+		<div className='container'>
 			<h1>Create Post</h1>
 			<CreatePost />
+			<hr />
+			<h1>Posts</h1>
+			<PostList />
 		</div>
 	)
 }
