3d3ee5ed283c770d031f3333215ab16530dfec75
Juan Feris <juan.feris@gorillalogic.com>
2021-04-24 12:35:52 -0500

    Handling Moderation

--- moderation/index.js
+++ moderation/index.js
@@ -4,7 +4,29 @@ import express from 'express'
 const app = express()
 app.use(express.json())
 
-app.post('/events', (req, res) => {})
+const wordToModerate = 'red'
+
+app.post('/events', async (req, res) => {
+	const { type, data } = req.body
+
+	if (type === 'CommentCreated') {
+		const status = data.content.includes(wordToModerate)
+			? 'rejected'
+			: 'approved'
+
+		await axios.post('http://localhost:4005/events', {
+			type: 'ModeratedComment',
+			data: {
+				id: data.id,
+				postId: data.postId,
+				status,
+				content: data.content,
+			},
+		})
+	}
+
+	res.send({})
+})
 
 app.listen(4003, () => {
 	console.log('Listening on 4003')
